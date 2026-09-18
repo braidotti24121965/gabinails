2867e55308283f0c10d58083a057d0b5579469f9
GabiNails <braidotti1965@gmail.com>
2026-09-18 00:45:32 -0300

    chore: cleanup scratch files

--- fix-client-modal-errors.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/nail-studio-app.tsx', 'utf8');
-
-code = code.replace('Calendar,,', 'Calendar,');
-
-fs.writeFileSync('src/components/nail-studio-app.tsx', code);
